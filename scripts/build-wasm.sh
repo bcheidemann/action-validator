d6b4ef455946eeefa8e911e3ae2e5c3531e546d0
Ben Heidemann <ben@heidemann.dev>
2025-10-09 14:40:52 +0100

    chore: remove support for WASM_PACK_BUILD_FLAGS in build-wasm.sh
    See
    https://github.com/mpalmer/action-validator/pull/86#discussion_r2412111605

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -2,9 +2,14 @@
 
 set -euo pipefail
 
-# [SC2086] Intenitonally omitting double quotes to allow word splitting of WASM_PACK_BUILD_FLAGS
-# shellcheck disable=SC2086
-npx wasm-pack build ${WASM_PACK_BUILD_FLAGS:-} --out-dir target/wasm-pack/build --no-typescript --target nodejs --no-default-features --features js
+WASM_PACK_FLAGS=""
+
+# Parse command line arguments
+if [ "${1:-}" = "--dev" ]; then
+  WASM_PACK_FLAGS="--no-opt"
+fi
+
+npx wasm-pack build $WASM_PACK_FLAGS --out-dir target/wasm-pack/build --no-typescript --target nodejs --no-default-features --features js
 rm -rf packages/core/snippets
 cp -R target/wasm-pack/build/snippets packages/core/snippets
 cp target/wasm-pack/build/action_validator_bg.wasm packages/core/
